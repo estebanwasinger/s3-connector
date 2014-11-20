95564ace9a5d77aae2c634d73866384dfb664a00
estebanwasinger <estebanwasinger@gmail.com>
2014-11-20 18:24:17 -0300

    Removed MetaDataSwitch

--- src/main/java/org/mule/module/s3/S3Connector.java
+++ src/main/java/org/mule/module/s3/S3Connector.java
@@ -43,8 +43,7 @@ import static org.mule.module.s3.util.InternalUtils.coalesce;
  * 
  * @author MuleSoft, Inc.
  */
-@Connector(name = "s3", schemaVersion = "2.0", friendlyName = "Amazon S3", minMuleVersion = "3.5",
-        metaData = MetaDataSwitch.OFF)
+@Connector(name = "s3", schemaVersion = "2.0", friendlyName = "Amazon S3", minMuleVersion = "3.5")
 public class S3Connector
 {
     /**
